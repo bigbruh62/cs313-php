20e3bc849d89a8a124c11c20cb259b203e1b540b
Nick <nicolasw.palmer@gmail.com>
2018-09-24 19:38:38 -0400

    added more template code

--- web/hello.php
+++ web/hello.php
@@ -18,9 +18,22 @@
 
 		<nav class="navbar navbar-expand-smbg-dark navbar-dark">
 			<a class="navbar-home" href="#">Home</a>
-			<div>
-				<a class="navbar-link" href="assignments.html">Assignments</a>				
-			</div>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    			<span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+	    		<ul class="navbar-nav">
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">Link</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">Link</a>
+	      			</li>
+	      			<li class="nav-item">
+	        		<a class="nav-link" href="#">Link</a>
+	      			</li>    
+	    		</ul>
+  			</div>
 
 		</nav>
 
