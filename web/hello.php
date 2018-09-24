680da7c0f11a22c45d92fde3a3a92657d8d64b8f
Nick <nicolasw.palmer@gmail.com>
2018-09-24 11:45:44 -0400

    actually added the new files

---
+++ web/hello.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="hello.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	</head>
+	<body>
+		<div class="jumbotron text-center">
+			<h1>Hellow World!</h1>
+			<p>A landing page for CS 313</p>
+		</div>
+
+		<nav class="navbar navbar-expand-smbg-dark navbar-dark">
+			<a class="navbar-home" href="#">Home</a>
+			<a class="navbar-link" href="assignments.html">Assignments</a>
+		</nav>
+
+		
+	</body>
+</html>
