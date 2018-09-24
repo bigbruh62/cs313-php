02f740e0fad801c3916f267b4c7516ea9680c9c7
Nick <nicolasw.palmer@gmail.com>
2018-09-24 10:41:16 -0400

    added debugging alert

--- web/clicked.js
+++ web/clicked.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$("#btnChangeColor").click(function(){
-		var color = $("#divbackground").value;
+		var color = $("#divBackground").value;
+		alert(color);
 		$("#div1").css("background-color", color);
 	})
 
