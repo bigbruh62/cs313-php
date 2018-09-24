2fe4ec1efdd4fe97444fe677d08abc051b6d2136
Nick <nicolasw.palmer@gmail.com>
2018-09-24 10:54:47 -0400

    added initial stretch 2 solution

--- web/clicked.js
+++ web/clicked.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	$("#btnChangeColor").click(function(){
 		var color = $("#divBackground").val();
-		alert(color);
 		$("#div1").css("background-color", color);
 	})
 
 	$("#btnClickMe").click(function(){
 		alert("Clicked!");
+	})
+
+	$("#btnFadeChange").click(function(){
+		$("#div3").fadeToggle("slow")
 	})
 });
