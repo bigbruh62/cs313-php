9968cacc0da5b7cb079a6ba77330ece3b68c5e74
Nick <nicolasw.palmer@gmail.com>
2018-09-24 09:05:51 -0400

    added js function to change background color of div1

--- web/clicked.js
+++ web/clicked.js
@@ -1,3 +1,9 @@
 function clicked() {
 	alert("Clicked!");
 }
+
+function changeColor() {
+	var textbox = document.getElementById("divbackground");
+
+	document.getElementById("div1").style.background-color = textbox.value;
+}
