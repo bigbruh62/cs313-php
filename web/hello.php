ddb95a1471743b267c2a62f14e2ec91b9695cf21
Nick <nicolasw.palmer@gmail.com>
2018-09-26 20:01:39 -0400

    Added bootstrap js

--- web/hello.php
+++ web/hello.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="hello.css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="jumbotron text-center">
