eed41d88fd6ebb774a4e892e5b3ac79b856434c0
Nick <nicolasw.palmer@gmail.com>
2018-09-26 21:27:17 -0400

    fixed link

--- web/header.php
+++ web/header.php
@@ -30,14 +30,14 @@
 </div>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">Assignments</a>
+        <a class="nav-link" href="assignments.php">Assignments</a>
       </li> 
     </ul>
   </div>  
