72053bb1b291dd72811c0ef05f7e822f5e15259b
Nick <nicolasw.palmer@gmail.com>
2018-09-27 13:20:16 -0400

    Trying to fix picture not showing up

--- web/hello.php
+++ web/hello.php
@@ -8,7 +8,7 @@
   <div class="row">
     <div class="col-sm-8">
       <h2>TITLE HEADING</h2>
-      <img src="dav.png">
+      <img src=".\dav.png">
       <h5>Title description, Dec 7, 2017</h5>
       <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
     </div>
