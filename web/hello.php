e81ec1a78570ab8a251654e5ce80d698ae47d112
Nick <nicolasw.palmer@gmail.com>
2018-09-27 13:36:17 -0400

    Fixed picture and added info about the charity

--- web/hello.php
+++ web/hello.php
@@ -7,10 +7,13 @@
 <div class="container" style="margin-top:30px">
   <div class="row">
     <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
+      <h2>
+      	Disabled American Veterans Charity
+      </h2>
       <img src=".\dav.png">
-      <h5>Title description, Dec 7, 2017</h5>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>
+      	The Disable American Veterans Charity (DAV) provides lifetime support for veterans and their families. THis support varies from helping veterans get to and from medical appointments to helping them with benefits claims to helping veterans find meaningful employment. Our veterans have sacrificed much to ensure that we and others are able to enjoy the rights and freedoms that all people should be able to enjoy.
+      </p>
     </div>
   </div>
 </div>
