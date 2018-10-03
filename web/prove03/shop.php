51949b65cfde9d84a0f9b7d8dfa377d84a2d1c4e
Nick <nicolasw.palmer@gmail.com>
2018-10-03 15:13:46 -0400

    added a php file that covers the header of the shop pages.

--- web/prove03/shop.php
+++ web/prove03/shop.php
@@ -2,9 +2,18 @@
 <html>
 <head>
 	<title>Tech Future Computer Shop</title>
-	
-</head>
-<body>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<? include("shop_header.php"); ?>
+	
 </body>
 </html>
---
+++ web/prove03/shop_header.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center">
+  <h1>
+    Tech Future Computer Shop
+  </h1>
+  <p id="landing">
+    We carry the best in computers and components
+  </p> 
+</div>
