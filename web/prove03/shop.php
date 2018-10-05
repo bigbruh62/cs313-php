2bfd67ce55f10f3c3abf0e9f4927bb9296c57d08
Nick <nicolasw.palmer@gmail.com>
2018-10-05 17:11:26 -0400

    restarted project, added needed files

--- web/prove03/shop.php
+++ web/prove03/shop.php
@@ -1,5 +1,11 @@
-<?
+<?php
 	session_start();
+
+	$cartArray = array();
+
+	if(!isset($_SESSION['cart'])){
+		$_SESSION['cart'] = $cartArray;		
+	}
 ?>
 
 <!DOCTYPE html>
@@ -14,87 +20,20 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$('.product').click(function() {
-				$.ajax({
-					type:"POST",
-					url:"some.php",
-					data: {}
-				}).done(function() {
-					alert( "Item added to cart")
-				});
-			});
-		});
-	</script>
 </head>
 
 <body>
 	<? include("shop_header.php"); ?>
-	<div class="container">
-		<div class="row">
-			<div class="col" style="overflow: auto; max-height: 100vh;">
-				<table>
-					<tr>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 1 Name
-							<button id="product1" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 2 Name
-							<button id="product2" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 3 Name
-							<button id="product3" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 4 Name
-							<button id="product4" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 5 Name
-							<button id="product5" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-						<td colspan="2" rowspan="2">
-							<img src=".\image-not-found.png" height="126" width="126">
-							</br>
-							Product 6 Name
-							<button id="product6" class="product" value="product1">
-								Add to cart
-							</button>
-						</td>
-					</tr>
-				</table> 
-			</div>
-		</div>
-		<div class="row">
-			<button id="cart">
-				Shopping Cart
-			</button>
-		</div>
+
+	<div>
+		<button id="product1" class="product" value="product 1">Add Product 1 to Cart</button>
+		<button id="product2" class="product" value="product 2">Add Product 2 to Cart</button>
+		<button id="product3" class="product" value="product 3">Add Product 3 to Cart</button>
+		<button id="product4" class="product" value="product 4">Add Product 4 to Cart</button>
+	</div>
+	<div>
+		<button id="toCart">Shopping Cart</button>
+		<button id="checkout">Checkout</button>
 	</div>
 
 </body>
