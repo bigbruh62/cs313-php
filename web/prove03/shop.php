92c28b9e54441e1746fe2890500f8ca52b5f04c3
Nick <nicolasw.palmer@gmail.com>
2018-10-05 16:06:20 -0400

    Added jquery function for the button click

--- web/prove03/shop.php
+++ web/prove03/shop.php
@@ -1,3 +1,7 @@
+<?
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,12 +14,25 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.product').click(function() {
+				$.ajax({
+					type:"POST",
+					url:"some.php",
+					data: {}
+				}).done(function() {
+					alert( "Item added to cart")
+				});
+			});
+		});
+	</script>
 </head>
 
 <body>
 	<? include("shop_header.php"); ?>
-	<div class="container"> Products
-		<div class="row"> row
+	<div class="container">
+		<div class="row">
 			<div class="col" style="overflow: auto; max-height: 100vh;">
 				<table>
 					<tr>
@@ -23,7 +40,7 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 1 Name
-							<button id="product1">
+							<button id="product1" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
@@ -31,7 +48,7 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 2 Name
-							<button id="product2">
+							<button id="product2" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
@@ -39,7 +56,7 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 3 Name
-							<button id="product3">
+							<button id="product3" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
@@ -49,7 +66,7 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 4 Name
-							<button id="product4">
+							<button id="product4" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
@@ -57,7 +74,7 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 5 Name
-							<button id="product5">
+							<button id="product5" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
@@ -65,13 +82,18 @@
 							<img src=".\image-not-found.png" height="126" width="126">
 							</br>
 							Product 6 Name
-							<button id="product6">
+							<button id="product6" class="product" value="product1">
 								Add to cart
 							</button>
 						</td>
 					</tr>
 				</table> 
 			</div>
+		</div>
+		<div class="row">
+			<button id="cart">
+				Shopping Cart
+			</button>
 		</div>
 	</div>
 
