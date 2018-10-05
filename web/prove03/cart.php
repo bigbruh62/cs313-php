51459c9ae6f527c599cb4a320558da92aa4acc45
Nick <nicolasw.palmer@gmail.com>
2018-10-05 19:04:16 -0400

    added skeleton to other pages

---
+++ web/prove03/cart.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cart</title>
+</head>
+<body>
+	<h1>Welcome to the cart</h1>
+</body>
+</html>
---
+++ web/prove03/confirmation.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmation</title>
+</head>
+<body>
+	<h1>Confirmation Page</h1>
+</body>
+</html>
