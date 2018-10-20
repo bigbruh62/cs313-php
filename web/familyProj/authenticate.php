321ecd446f8b7d17834357966c880ef0fc8aea73
Nick <nicolasw.palmer@gmail.com>
2018-10-20 08:50:54 -0400

    trying to get the authentication working

--- web/familyProj/authenticate.php
+++ web/familyProj/authenticate.php
@@ -1,22 +1,5 @@
 <!DOCTYPE html>
-<?php
-  require '..\dbCredentials.php'
 
-  $user = $_POST['username'];
-  $pswd = $_POST['password'];
-  
-  $qry = $db -> prepare('SELECT users.id, users.username FROM users WHERE username=:user AND pswdhash = :pswd');
-  $qry -> execute([':user' => $user, ':pswd' => $pswd]);
-  $results = $qry -> fetchAll(PDO::FETCH_ASSOC);
-
-  if ($results) {
-    alert("inside the results if statement");
-    session_start();
-    $_SESSION['user'] = $results[0]['id'];
-    $_SESSION['auth'] = TRUE;
-    header('Location: home.php');
-  }
-?>
 <html>
 <head>
   <meta charset="utf-8"/>
@@ -28,6 +11,24 @@
 </head>
 <body>
 <div class="container">
+  <?php
+  require '..\dbCredentials.php'
+
+  $user = $_POST['username'];
+  $pswd = $_POST['password'];
+  
+  $qry = $db -> prepare("SELECT users.id, users.username FROM users WHERE username=:user AND pswdhash = :pswd");
+  $qry -> execute([':user' => $user, ':pswd' => $pswd]);
+  $results = $qry -> fetchAll(PDO::FETCH_ASSOC);
+
+  if ($results) {
+    alert("inside the results if statement");
+    session_start();
+    $_SESSION['user'] = $results[0]['id'];
+    $_SESSION['auth'] = TRUE;
+    header('Location: home.php');
+  }
+?>
   <div>
     <?php
       echo '<p>Username or password incorrect.</p>';
