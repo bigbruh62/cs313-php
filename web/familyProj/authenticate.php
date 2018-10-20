0e0bddb667a148abd5f474fb3815c671335796a0
Nick <nicolasw.palmer@gmail.com>
2018-10-20 18:26:26 -0400

    added code from test file to authenticate

--- web/familyProj/authenticate.php
+++ web/familyProj/authenticate.php
@@ -1,16 +1,46 @@
 <!DOCTYPE html>
 <?php
-  include("..\dbCredentials.php");
+  //include("..\dbCredentials.php");
 
-  $user = $_POST['username'];
-  $pswd = $_POST['password'];
+  //$user = $_POST['username'];
+  //$pswd = $_POST['password'];
   
   //$qry = $db -> prepare('SELECT id, username FROM users WHERE username=user1 AND pswdhash = pass');
   //$qry -> execute(['user' => $user, 'pswd' => $pswd]);
   //$results = $qry -> fetchAll(PDO::FETCH_ASSOC);
-  $results = $db->query('SELECT id, username FROM users');
+  //$results = $db->query('SELECT id, username FROM users');
 
-  if ($results) {
+    try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+
+  $user = 'user1';
+  $pswd = 'pass';
+
+  $stmnt = $db->prepare("SELECT id, username FROM users WHERE username = ? AND pswdhash = ?");
+  $stmnt->execute([$user, $pswd]);
+  $stmnt = $stmnt->fetch(PDO::FETCH_ASSOC);
+
+
+  if ($stmnt) {
     alert("inside the results if statement");
     session_start();
     $_SESSION['user'] = $results[0]['id'];
