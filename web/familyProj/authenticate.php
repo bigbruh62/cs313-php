801fe954a05dd4c19a7890947354a3b881f99e1e
Nick <nicolasw.palmer@gmail.com>
2018-10-20 16:23:46 -0400

    trying to get the authentication working

--- web/familyProj/authenticate.php
+++ web/familyProj/authenticate.php
@@ -5,17 +5,18 @@
   $user = $_POST['username'];
   $pswd = $_POST['password'];
   
-  $qry = $db -> prepare('SELECT * FROM users WHERE username=:user AND pswdhash = :pswd');
-  $qry -> execute(['user' => $user, 'pswd' => $pswd]);
-  $results = $qry -> fetchAll(PDO::FETCH_ASSOC);
+  //$qry = $db -> prepare('SELECT id, username FROM users WHERE username=user1 AND pswdhash = pass');
+  //$qry -> execute(['user' => $user, 'pswd' => $pswd]);
+  //$results = $qry -> fetchAll(PDO::FETCH_ASSOC);
+  $results = $db->query('SELECT id, username FROM users WHERE username=user1 AND pswdhash=pass');
 
-  //if ($results) {
-    //alert("inside the results if statement");
-    //session_start();
-    //$_SESSION['user'] = $results[0]['id'];
-    //$_SESSION['auth'] = TRUE;
-    //header('Location: home.php');
-  //}
+  if ($results) {
+    alert("inside the results if statement");
+    session_start();
+    $_SESSION['user'] = $results[0]['id'];
+    $_SESSION['auth'] = TRUE;
+    header('Location: home.php');
+  }
 ?>
 <html>
 <head>
