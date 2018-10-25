b1dd3717311393aceabb3b23c135f50354bf92f0
Nick <nicolasw.palmer@gmail.com>
2018-10-25 12:45:42 -0400

    made changes to the layout of the add member page

--- web/familyProj/addMember.php
+++ web/familyProj/addMember.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Add Member</title>
@@ -12,7 +11,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-light">
 	<div class="row">
 		<div class="col-3"></div>
 		<dir class="col-6">
@@ -26,17 +25,16 @@
 			<form class="panel" action="add.php" method="post">
 				<span>
 					<h2>First Name</h2>
-					</br>
 					<input type="text" name="first">
 				</span>
+			</br>
 				<span>
 					<h2>Last Name</h2>
-					</br>
 					<input type="text" name="last">
 				</span>
+			</br>
 				<span>
 					<h2>Birthday</h2>
-					</br>
 					<input type="text" name="date">
 				</span>	
 			</form>
