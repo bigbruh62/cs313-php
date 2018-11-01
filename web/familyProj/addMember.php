fd207f9560117f96a3d50a2ac293c6e9ac2179e8
Nick <nicolasw.palmer@gmail.com>
2018-11-01 11:50:35 -0400

    added css sheet

--- web/familyProj/addMember.php
+++ web/familyProj/addMember.php
@@ -7,13 +7,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href=".\style.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body class="bg-light">
-	<div class="row">
+	<div class="row header">
 		<div class="col-3"></div>
 		<dir class="col-6">
 			<h1>Add a family member</h1>
