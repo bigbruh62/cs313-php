a5a598d57e2e07e2cbab5c68bab3e4bc80b088da
Nick <nicolasw.palmer@gmail.com>
2018-11-01 11:08:01 -0400

    stylized the layout of the login page

--- web/familyProj/login.php
+++ web/familyProj/login.php
@@ -20,36 +20,38 @@
 		<div class="col-6"></div>
 		<div class="col-3"></div>
 	</div>
-	<div class="row align-items-center">
-		<div class="col-3"></div>
-		<div class="col-6">
-			<form class="container" action="authenticate.php" method="post">
-					<span class="container">
-						Account Login
-					</span>
-					</br>
-					<span class="container">
-						Username
-					</span>
-					<div class="container" data-validate = "Username is required">
-						<input type="text" name="username" id="username" placeholder="username">
-						<span ></span>
-					</div>
-					
-					<span class="container">
-						Password
-					</span>
-					<div class="container" data-validate = "Password is required">
-						<span ></span>
-						<input type="password" name="password" id="password">
-						<span class="glyphicon glyphicon-eye-open"></span>
-					</div>
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-3"></div>
+			<div class="col-6">
+				<form class="container" action="authenticate.php" method="post">
+						<span class="container">
+							Account Login
+						</span>
+						</br>
+						<span class="container">
+							Username
+						</span>
+						<div class="container" data-validate = "Username is required">
+							<input type="text" name="username" id="username" placeholder="username">
+							<span ></span>
+						</div>
+						
+						<span class="container">
+							Password
+						</span>
+						<div class="container" data-validate = "Password is required">
+							<span ></span>
+							<input type="password" name="password" id="password">
+							<span class="glyphicon glyphicon-eye-open"></span>
+						</div>
 
-					<div class="container">
-						<input class="btn" type="submit" value="Login">
-					</div>
-				</form>
+						<div class="container">
+							<input class="btn" type="submit" value="Login">
+						</div>
+					</form>
+			</div>
+			<div class="col-3"></div>
 		</div>
-		<div class="col-3"></div>
 </body>
 </html>
